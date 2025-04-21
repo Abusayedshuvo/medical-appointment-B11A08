239d46bebc455bf490ad93eb6b7c41d021c5f2e1
Abusayedshuvo <abusayedshuvo24@gmail.com>
2025-04-21 23:08:46 +0600

    Navbar and Banner is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Banner from "./component/Banner/Banner";
+import Navbar from "./component/Navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </>
+  );
 }
 
 export default App;
